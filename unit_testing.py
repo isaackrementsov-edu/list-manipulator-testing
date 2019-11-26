7855c30eed0d07b9af92ba55ceb71fc02214b3ee
= <=>
2019-11-26 00:07:52 +0000

    Created the program skeleton with three test case classes

---
+++ unit_testing.py
@@ -0,0 +1,13 @@
+import unittest;
+
+
+class TestListManipulatorMin(unittest.TestCase):
+
+
+class TestListManipulatorMax(unittest.TestCase):
+
+
+class TestListManipulatorRemove(unittest.TestCase):
+
+
+unittest.main()
